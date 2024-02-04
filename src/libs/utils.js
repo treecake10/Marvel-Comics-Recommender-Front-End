c34ded7c71f837fbd81c93ad86343813972dd3b1
Nick <ntibbet1@gmail.com>
2024-02-04 15:15:15 -0500

    updating utils.js

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -2,65 +2,55 @@ import MD5 from 'crypto-js/md5'
 import availableEventIds from '../eventList.json'
 
 const API_URL = process.env.REACT_APP_BASE_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+const privateKey = process.env.REACT_APP_PRIVATE_KEY;
 
-const getHash = (ts, secretKey, publicKey) => {
-    return MD5(ts + secretKey + publicKey).toString()
-}
+const getHash = (ts) => MD5(ts + privateKey + apiKey).toString();
 
 const fetchComicEvents = async () => {
-
-  let eventUrl = `${API_URL}/v1/public/events`
-
-  let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
-  let hash = getHash(timeStamp, privateKey, apiKey)
+  const eventUrl = `${API_URL}/v1/public/events`;
+  const timeStamp = Date.now().toString();
+  const hash = getHash(timeStamp, privateKey, apiKey);
 
   console.log(timeStamp);
   console.log(hash);
 
   const numRandomEvents = 9;
 
-  const eventsData = [];
-
   const shuffledEventIds = [...availableEventIds].sort(() => 0.5 - Math.random());
   const selectedEventIds = shuffledEventIds.slice(0, Math.min(numRandomEvents, shuffledEventIds.length));
 
-  for (let i = 0; i < Math.min(numRandomEvents, selectedEventIds.length); i++) {
+  const eventsData = await Promise.all(
+    selectedEventIds.map(async ({ value: eventId }) => {
+      const params = new URLSearchParams({
+        apikey: apiKey,
+        ts: timeStamp,
+        hash: hash,
+      });
 
-    const eventId = selectedEventIds[i].value;
+      const url = `${eventUrl}/${eventId}?${params.toString()}`;
 
-    const params = new URLSearchParams({
-      apikey: apiKey,
-      ts: timeStamp,
-      hash: hash,
-    });
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    const url = `${eventUrl}/${eventId}?${params.toString()}`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data.data && data.data.results) {
-        eventsData.push(data.data.results[0]);
+        if (data.data && data.data.results) {
+          return data.data.results[0];
+        }
+      } catch (error) {
+        console.error(`Error fetching data for event ID ${eventId}:`, error);
       }
-    } catch (error) {
-      console.error(`Error fetching data for event ID ${eventId}:`, error);
-    }
-    
-  }
+      return null;
+    })
+  );
 
-  return eventsData 
-
+  return eventsData.filter((event) => event !== null);
 };
 
 const fetchCharactersByName = async (name, offset) => {
   let characterUrl = `${API_URL}/v1/public/characters`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let limit = 100
   let hash = getHash(timeStamp, privateKey, apiKey)
 
@@ -82,8 +72,6 @@ const fetchCharacterById = async (id) => {
   let characterUrl = `${API_URL}/v1/public/characters/${id}`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let hash = getHash(timeStamp, privateKey, apiKey)
 
   let url = `${characterUrl}?ts=${timeStamp}&apikey=${apiKey}&hash=${hash}`;
@@ -104,8 +92,6 @@ const fetchSeriesByCharacterId = async (id, offset) => {
   let seriesUrl = `${API_URL}/v1/public/characters/${id}/series`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let limit = 100
   let hash = getHash(timeStamp, privateKey, apiKey)
 
@@ -127,8 +113,6 @@ const fetchCreatorsByName = async (name, offset) => {
   let creatorUrl = `${API_URL}/v1/public/creators`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let limit = 100
   let hash = getHash(timeStamp, privateKey, apiKey)
 
@@ -150,8 +134,6 @@ const fetchCreatorById = async (id) => {
   let creatorUrl = `${API_URL}/v1/public/creators/${id}`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let hash = getHash(timeStamp, privateKey, apiKey)
 
   let url = `${creatorUrl}?ts=${timeStamp}&apikey=${apiKey}&hash=${hash}`;
@@ -172,8 +154,6 @@ const fetchSeriesByCreatorId = async (id, offset) => {
   let storiesUrl = `${API_URL}/v1/public/creators/${id}/series`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let limit = 100
   let hash = getHash(timeStamp, privateKey, apiKey)
 
@@ -195,8 +175,6 @@ const fetchEventsByCreatorId = async (id, offset) => {
   let eventsUrl = `${API_URL}/v1/public/creators/${id}/events`;
 
   let timeStamp = Date.now().toString()
-  let apiKey = process.env.REACT_APP_API_KEY
-  let privateKey = process.env.REACT_APP_PRIVATE_KEY
   let limit = 100
   let hash = getHash(timeStamp, privateKey, apiKey)
 
